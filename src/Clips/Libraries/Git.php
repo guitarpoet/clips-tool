3fcae8df8a4c99ae6018085bf8754b87784d16ed
Jack <guitarpoet@gmail.com>
2015-01-14 19:23:27 +0800

    Added the git processing tool

---
+++ src/Clips/Libraries/Git.php
@@ -0,0 +1,18 @@
+<?php namespace Clips\Libraries; in_array(__FILE__, get_included_files()) or exit("No direct sript access allowed");
+
+class Git implements \Psr\Log\LoggerAwareInterface {
+	public function repo($path) {
+		return new \Gitonomy\Git\Repository($path, array('logger' => $this->logger));
+	}
+
+	public function setLogger(\Psr\Log\LoggerInterface $logger) {
+		$this->logger = $logger;
+	}
+
+	public function getHeadCommit($path) {
+		$repo = $this->repo($path);
+		$head = $repo->getHead();
+		$rev = ($repo->getRevision($head->getFullname()));
+		return $rev->getCommit();
+	}
+}
---
+++ tests/Clips/Libraries/GitTest.php
@@ -0,0 +1,16 @@
+<?php in_array(__FILE__, get_included_files()) or exit("No direct sript access allowed");
+
+class GitTest extends Clips\TestCase {
+	public function doSetUp() {
+		$this->tool = &get_clips_tool();
+		$this->git = $this->tool->library('git');
+	}
+
+	public function testGetInfo() {
+		$this->assertNotNull($this->git);
+		$repo = ($this->git->repo(clips_path('/../../../')));
+		$head = $repo->getHead();
+		$rev = ($repo->getRevision($head->getFullname()));
+		echo $rev->getCommit()->getCommitterDate()->getTimestamp();
+	}
+}
