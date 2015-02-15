9b20d904576e06d2884d7767e463d6e2c1afa94e
Jack <guitarpoet@gmail.com>
2015-02-15 18:33:56 +0800

    Added the pagination support class.

---
+++ src/Clips/Pagination.php
@@ -0,0 +1,141 @@
+<?php namespace Clips; in_array(__FILE__, get_included_files()) or exit("No direct sript access allowed");
+
+/**
+ * The pagination support object
+ */
+class Pagination {
+
+	/**
+	 * The select fields
+	 */
+	public $select = '*';
+
+	/**
+	 * The total records of this type in database
+	 */
+	public $dbtotal;
+
+	/**
+	 * The total records count
+	 */
+	public $total;
+
+	/**
+	 * The current record offset 
+	 */
+	public $offset;
+
+	/**
+	 * The total page item array length
+	 */
+	public $length;
+
+	/**
+	 * The items
+	 */
+	public $items;
+
+	/**
+	 * The join options
+	 */
+	public $joins = array();
+
+	/**
+	 * The order by parameters
+	 */
+	public $orderBy = array();
+
+	/**
+	 * The where parameters
+	 */
+	public $where = array();
+
+	/**
+	 * The tag object for carrying data between queries
+	 */
+	public $tag = null; 
+
+	/**
+	 * The query object for query pagination using queries
+	 *
+	 * The query object has these fields (query, total_query, filtered_query)
+	 */
+	public $customized_query = null;
+
+	public function __construct($total = 0, $offset = 0, $length = -1) {
+		if($length != -1)
+			$this->length = $length;
+		else {
+			$length = clips_config('pagination_length');
+			if($length)
+				$this->length = $length[0];
+		}
+		$this->offset = $offset;
+	}
+
+	public function first() {
+		$this->offset = 0;
+	}
+
+	public function last() {
+		$this->offset = ($this->totalCount() - 1) * $this->length;
+	}
+
+	public function page($page) {
+		if($page <= 0 || $page > $this->totalCount()) {
+			return false;
+		}
+		$this->offset = ($page - 1) * $this->length;
+		return $page;
+	}
+
+	public function next() {
+		if($this->hasNext()) {
+			$this->offset = ($this->current() + 1) * $this->length;
+			return $this->current();
+		}
+		return false;
+	}
+
+	public function prev() {
+		if($this->hasPrev()) {
+			$this->offset = ($this->current() - 1) * $this->length;
+			return $this->current();
+		}
+		return false;
+	}
+
+	public function hasNext() {
+		return $this->current() < $this->totalCount();
+	}
+
+	public function hasPrev() {
+		return $this->offset - $this->length > 0;
+	}
+
+	public function totalCount() {
+		if($this->length) {
+			return ceil($this->total / $this->length);
+		}
+		return 0;
+	}
+
+	public function current() {
+		if($this->length) {
+			$ret = ceil($this->offset / $this->length);
+			return $ret? $ret: 1;
+		}
+		return 0;
+	}
+
+	public static function fromJson($json) {
+		$obj = (array) parse_json($json);
+		$ret = new PaginationSupport();
+		copyArray2Obj($obj, $ret);
+		return $ret;
+	}
+
+	public function toJson() {
+		return json_encode($this);
+	}
+}
