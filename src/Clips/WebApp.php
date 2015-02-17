e4f9c71d824c128068976dc885326fcf0a8cf8e2
Jack <guitarpoet@gmail.com>
2015-02-17 10:42:45 +0800

    Added the web application facade.

---
+++ src/Clips/WebApp.php
@@ -0,0 +1,17 @@
+<?php namespace Clips; in_array(__FILE__, get_included_files()) or exit("No direct sript access allowed"); 
+
+/**
+ * The web application facade
+ *
+ * @author Jack <guitarpoet@gmail.com>
+ * @date Tue Feb 17 10:34:35 2015
+ */
+class WebApp {
+	public function __construct($name = '') {
+		$this->tool = get_clips_tool();
+		clips_context('app', $this);
+		$this->name = $name;
+		$this->router = $this->tool->load_class('Router', true);
+		$this->router->route();
+	}
+}
