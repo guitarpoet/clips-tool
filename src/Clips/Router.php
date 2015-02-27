9e811d2a86df75366bbc7783266fe2b928dad33e
Jack <guitarpoet@gmail.com>
2015-02-27 13:28:22 +0800

    Fixed the router bug.

--- src/Clips/Router.php
+++ src/Clips/Router.php
@@ -151,7 +151,7 @@ class Router implements LoggerAwareInterface, ClipsAware, ToolAware {
 					$this->tool->enhance($a);
 					$controller->bundle = $a;
 					if(isset($a->name))
-						Clips\context('current_bundle', $a->name);
+						context('current_bundle', $a->name);
 				}
 				else if(get_class($a) == 'Clips\\Meta') {
 					if(isset($a->value) && is_array($a->value)) {
