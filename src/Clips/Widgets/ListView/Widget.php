045aaaa6b64b2cf2efea55ab21cf431c62418ef7
andy <andy.ling@pinet.co>
2015-02-28 14:03:51 +0800

    Change listview bug

--- src/Clips/Widgets/ListView/Widget.php
+++ src/Clips/Widgets/ListView/Widget.php
@@ -1,4 +1,22 @@
 <?php namespace Clips\Widgets\ListView; in_array(__FILE__, get_included_files()) or exit("No direct sript access allowed");
 
 class Widget extends \Clips\Widget {
+	protected function doInit() {
+		$js = <<<TEXT
+			$('.listview.clips-listview').on('list.loaded', function(){
+				var self = $(this);
+				if($.isFunction($.fn.selectBoxIt)){
+					self.parents('.listview_wrapper').find('select:not([data-no-selectBoxIt])').each(function(){
+						console.log($(this));
+						$(this).selectBoxIt({});
+					});
+//					$(this).parentsUntil('.listview_wrapper').text(1);
+//					$(this).parentsUntil('.listview_wrapper').find('select:not([data-no-selectBoxIt])').each(function(){
+//						$(this).selectBoxIt({});
+//					});
+				}
+			});
+TEXT;
+		\Clips\context('jquery_init', $js, true);
+	}
 }
