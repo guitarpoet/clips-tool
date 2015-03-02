fb3a1a7a1c9e544601a3935bcfeafafe35c25f32
Jack <guitarpoet@gmail.com>
2015-03-02 15:29:06 +0800

    Added the customize render support for datatable column.

--- src/Clips/Widgets/DataTable/js/datatable.functions.js
+++ src/Clips/Widgets/DataTable/js/datatable.functions.js
@@ -1,13 +1,16 @@
 function datatable_action_column(data, type, row, meta) {
+	var func = $('#' + meta.settings.sTableId).attr('name') + '_datatable_action';
 	var col = meta.settings.aoColumns[meta.col];
 	var actionUrl = col.action;
+	if(typeof window[func] == 'function') {
+		return window[func].call(window, data, type, row, col, meta)
+	}
 	var refer = col.refer;
-	console.info('The refer for col ' + col.data + ' is ' + refer);
 	if(refer) {
 		if(actionUrl)
 			return "<a data-id='" + row[refer] + "' href='"+ Clips.siteUrl(actionUrl) + "/" + row[refer] +"'>"+ data +"</a>"
 		else
 			return "<a class='no_text_decoration' data-id='" + row[refer] + "'>"+ data +"</a>"
 	}
-	return "<a href='"+ CLips.siteUrl(actionUrl) + "/" + data +"'>"+ data +"</a>"
+	return "<a href='"+ Clips.siteUrl(actionUrl) + "/" + data +"'>"+ data +"</a>"
 }
