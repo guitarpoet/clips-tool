c0ef9ab5e987b09cdfb52a67275d6ae7625080d1
Jack <guitarpoet@gmail.com>
2015-03-03 16:14:13 +0800

    Added the submit value default i18n.

--- src/Clips/Widgets/Form/smarty/function.submit.php
+++ src/Clips/Widgets/Form/smarty/function.submit.php
@@ -4,10 +4,14 @@ function smarty_function_submit($params, $template) {
 	$bundle_name = Clips\context('current_bundle');
 	$bundle = Clips\get_default($params, 'bundle', $bundle_name);
 
-	$submit = 'Submit';
+	$submit = Clips\get_default($params, 'value', 'Submit');
 	if($bundle !== null) {
 		$bundle = Clips\bundle($bundle);
 		$submit = $bundle->message($submit);
 	}
+
+	if(isset($params['value']))
+		$params['value'] = $submit;
+
 	return Clips\create_tag('input', $params, array('type' => 'submit', 'value' => $submit, 'class' => array('btn', 'btn-primary')));
 }
