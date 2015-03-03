d09c1762771b5a37502b1f90cfe02e64547972f3
Jack <guitarpoet@gmail.com>
2015-03-03 16:11:12 +0800

    Added the default i18n support for input sumbit.

--- src/Clips/Widgets/Form/smarty/function.submit.php
+++ src/Clips/Widgets/Form/smarty/function.submit.php
@@ -1,5 +1,13 @@
 <?php in_array(__FILE__, get_included_files()) or exit("No direct sript access allowed");
 
 function smarty_function_submit($params, $template) {
-	return Clips\create_tag('input', $params, array('type' => 'submit', 'value' => 'Submit', 'class' => array('btn', 'btn-primary')));
+	$bundle_name = Clips\context('current_bundle');
+	$bundle = Clips\get_default($params, 'bundle', $bundle_name);
+
+	$submit = 'Submit';
+	if($bundle !== null) {
+		$bundle = Clips\bundle($bundle);
+		$submit = $bundle->message($submit);
+	}
+	return Clips\create_tag('input', $params, array('type' => 'submit', 'value' => $submit, 'class' => array('btn', 'btn-primary')));
 }
