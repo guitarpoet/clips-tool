906762b77adbc46d2de67f510e4f96e0ab1dcd70
Jack <guitarpoet@gmail.com>
2015-03-05 14:51:11 +0800

    Added the helper method for router to run route manually.

---
+++ tests/Clips/RouterTest.php
@@ -0,0 +1,23 @@
+<?php in_array(__FILE__, get_included_files()) or exit("No direct sript access allowed");
+
+/**
+ * @Clips\Object("router")
+ */
+class RouterTest extends Clips\TestCase {
+	public function testRouteResult() {
+		$result = ($this->router->routeResult('responsive/size/400/a.jpg'));
+		$this->assertEquals(count($result), 1);
+		$result = $result[0];
+		$this->assertTrue(Clips\valid_obj($result, 'Clips\\RouteResult'));
+		$this->assertEquals($result->controller, 'Clips\\Controllers\\ResponsiveController');
+		$this->assertEquals($result->method, 'size');
+		$this->assertEquals($result->args, array('400', 'a.jpg'));
+	}
+
+	public function testRouteError() {
+		$result = $this->router->routeResult('no/this/controller/');
+		$this->assertEquals(count($result), 1);
+		$result = $result[0];
+		$this->assertEquals($result[0], 'RouteError');
+	}
+}
