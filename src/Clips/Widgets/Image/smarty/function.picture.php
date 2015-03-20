2bffa0fb70854cabc22a11d7b035f0e438cb12fe
Jack <guitarpoet@gmail.com>
2015-03-20 16:08:10 +0800

    Added the listview template support for picture.

--- src/Clips/Widgets/Image/smarty/function.picture.php
+++ src/Clips/Widgets/Image/smarty/function.picture.php
@@ -2,68 +2,78 @@
 
 function smarty_function_picture($params, $template) {
 	$src = Clips\get_default($params, 'src');
-	$path = Clips\get_default($params, 'path', 'responsive/size');
-	$resolutions = Clips\get_default($params, 'resolutions');
-	$medias = Clips\get_default($params, 'medias');
 
-	$img_path = Clips\find_image($src);
-
-	if(!$img_path) {
-		Clips\error('figure', array('Can\'t find image '.$src.'!'));
-		return '';
-	}
-	$size = Clips\image_size($img_path);
-	$size = $size['width'];
-
-	$content = array();
-	$level = Clips\context('indent_level');
-	if($level === null)
-		$level = 0; // Default level is 0
-	else
-		$level = count($level);
-
-	$indent = '';
-	for($i = 0; $i < $level; $i++) {
-		$indent .= "\t";
-	}
-
-	if($resolutions || $medias) {
-
-		if($resolutions) { // If we are using auto resizing, skip the resolutions
-			unset($params['resolutions']);
-			foreach($resolutions as $res) {
-				$content []= "\t".Clips\create_tag('source', array(
-					'src' => Clips\site_url('responsive/size/'.(float)$res / 2880 * (float)$size .'/'.$src),
-					'media' => '(min-width:'.$res.'px)'
-				));
-			}
-		}
-
-		if($medias) {
-			unset($params['medias']);
-			foreach($medias as $media => $res) {
-				$content []= "\t".Clips\create_tag('source', array(
-					'src' => Clips\site_url('responsive/size/'.$res.'/'.$src),
-					'media' => '(min-width:'.$media.'px)'
-				));
-			}
+	if($src) {
+		if(preg_match('/_\\([A-Za-z0-9_]+\\)/', $src)) {
+			$template = true;
 		}
 	}
-	else
-		$params['path'] = Clips\site_url($path);
+	if(!isset($template)) {
+		$path = Clips\get_default($params, 'path', 'responsive/size');
+		$resolutions = Clips\get_default($params, 'resolutions');
+		$medias = Clips\get_default($params, 'medias');
 
-	$image_dir = Clips\config('image_dir');
-	if($image_dir) {
-		$image_dir = $image_dir[0];
+		$img_path = Clips\find_image($src);
+
+		if(!$img_path) {
+			Clips\error('picture', array('Can\'t find image '.$src.'!'));
+			return '';
+		}
+		$size = Clips\image_size($img_path);
+		$size = $size['width'];
+
+		$content = array();
+		$level = Clips\context('indent_level');
+		if($level === null)
+			$level = 0; // Default level is 0
+		else
+			$level = count($level);
+
+		$indent = '';
+		for($i = 0; $i < $level; $i++) {
+			$indent .= "\t";
+		}
+
+		if($resolutions || $medias) {
+			if($resolutions) { // If we are using auto resizing, skip the resolutions
+				unset($params['resolutions']);
+				foreach($resolutions as $res) {
+					$content []= "\t".Clips\create_tag('source', array(
+						'src' => Clips\site_url('responsive/size/'.(float)$res / 2880 * (float)$size .'/'.$src),
+						'media' => '(min-width:'.$res.'px)'
+					));
+				}
+			}
+
+			if($medias) {
+				unset($params['medias']);
+				foreach($medias as $media => $res) {
+					$content []= "\t".Clips\create_tag('source', array(
+						'src' => Clips\site_url('responsive/size/'.$res.'/'.$src),
+						'media' => '(min-width:'.$media.'px)'
+					));
+				}
+			}
+		}
+		else
+			$params['path'] = Clips\site_url($path);
+
+		$image_dir = Clips\config('image_dir');
+		if($image_dir) {
+			$image_dir = $image_dir[0];
+		}
+		else {
+			$image_dir = 'application/static/img/';
+		}
+		Clips\clips_context('indent_level', 1, true);
+		$img = Clips\create_tag('img', array('src' => Clips\static_url(Clips\path_join($image_dir, $src))));
+		$content []= "\t".Clips\create_tag_with_content('noscript', $img);
+		Clips\context_pop('indent_level');
 	}
 	else {
-		$image_dir = 'application/static/img/';
+		$params['data-role'] = 'item-picture';
+		$params['path'] = Clips\get_default($params, 'path', 'responsive/size');
+		$content = '';
 	}
-	Clips\clips_context('indent_level', 1, true);
-	$img = Clips\create_tag('img', array('src' => Clips\static_url(Clips\path_join($image_dir, $src))));
-	$content []= "\t".Clips\create_tag_with_content('noscript', $img);
-	Clips\context_pop('indent_level');
-
-
-	return Clips\create_tag_with_content('picture', implode("\n$indent", $content), $params);
+	return Clips\create_tag_with_content('picture', $content, $params);
 }
