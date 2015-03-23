364fc93be769060af0a85f49f97876483668a695
andy <andy.ling@pinet.co>
2015-03-23 19:24:37 +0800

    Change listview

--- src/Clips/Widgets/ListView/js/listview.js
+++ src/Clips/Widgets/ListView/js/listview.js
@@ -79,16 +79,15 @@
 				
 				list.pageLength = savedStates.length;
 				
-				if(!list.states.oldPagelength) {
-					list.states.oldPagelength = false;
-				}
-				
 				if(savedStates.search && savedStates.search.value)
 					list.search_value = savedStates.search.value;
 				if(savedStates.order && savedStates.order[0].column) {
 					list.orderColumn = savedStates.order[0].column;
 					list.orderDir = savedStates.order[0].dir;
 				}
+			}
+			else {
+				list.states = {};
 			}
 		}
 		
