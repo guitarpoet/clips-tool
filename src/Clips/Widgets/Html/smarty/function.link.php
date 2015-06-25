9db274ec3cf6a5b78507f9a174ffbec914a3a17d
Jack <guitarpoet@gmail.com>
2015-06-25 14:03:13 +0800

    Added static url support for link tag.

--- src/Clips/Widgets/Html/smarty/function.link.php
+++ src/Clips/Widgets/Html/smarty/function.link.php
@@ -1,5 +1,9 @@
 <?php in_array(__FILE__, get_included_files()) or exit("No direct script access allowed");
 
 function smarty_function_link($params, $template) {
+	$uri = Clips\get_default($params, 'uri');
+	if($uri) {
+		$params['href'] = Clips\static_url($uri);
+	}
 	return Clips\create_tag('link', $params);
 }
