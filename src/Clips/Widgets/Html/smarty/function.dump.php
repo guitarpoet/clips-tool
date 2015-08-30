fedf360b476f0d3759999087e7ef55fbe1b878f4
Jack <guitarpoet@gmail.com>
2015-08-30 21:02:41 +0800

    Refactor the dump to a more friendly version.

--- src/Clips/Widgets/Html/smarty/function.dump.php
+++ src/Clips/Widgets/Html/smarty/function.dump.php
@@ -1,9 +1,7 @@
 <?php in_array(__FILE__, get_included_files()) or exit("No direct script access allowed");
 
 function smarty_function_dump($params, $template) {
-	$obj = Clips\get_default($params, 'obj');
-
-	if($obj) {
-		var_dump($obj);
+	if($params) {
+		var_dump($params);
 	}
 }
